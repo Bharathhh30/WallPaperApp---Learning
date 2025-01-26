f8d562c2b101f70675b2ed9dd49957d85f106b3c
Bharathhh30 <vnsb30@gmail.com>
2025-01-26 23:24:57 +0530

    2:24:00 break, bottom sheets used(customisation left)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,23 +5,23 @@ export default function TabLayout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'orange' , headerShown:false }}>
       <Tabs.Screen
-        name="index"
+        name="foryou"
         options={{
           title: 'For you',
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Explore',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+          }}
+        />
       <Tabs.Screen
         name="account"
         options={{
           title: 'Account',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
         }}
       />
---
+++ hooks/useWallPapers.ts
@@ -0,0 +1,45 @@
+// https://ideogram.ai/assets/progressive-image/balanced/response/qsypaQtWQOiL6b4OA67jUA
+// https://ideogram.ai/assets/image/lossless/response/h5llb1nMR3eCDY4sAmZV7g
+
+
+export interface WallPaper{
+    url: string;
+    name: string;
+}
+
+export function useWallPapers() : WallPaper[]{
+    return [{
+        url: "https://ideogram.ai/assets/progressive-image/balanced/response/qsypaQtWQOiL6b4OA67jUA",
+        "name":"Heritage"
+    },
+    {
+        url: "https://ideogram.ai/assets/image/lossless/response/h5llb1nMR3eCDY4sAmZV7g",
+            "name":"Dusk"
+    },
+    {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/tjjzRNwzSACgUmYWFtFR2w",
+        "name":"Sunset"
+    },
+    {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/UhF8z7vIRb27Uiz1JO0dJQ",
+        "name":"SettingSun"
+    },
+    {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/LRGYVdUQS_iX4taH3bgZCA",
+        "name":"People"
+    },
+    {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/eNLVXFF2SaiPzPc-sRk9PQ",
+        "name":"Arty"
+    },
+    {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/uRtWabAQSymcg43ADbpWVg",
+        "name":"Pinky"
+    },
+    {
+        url:"https://ideogram.ai/assets/progressive-image/balanced/response/8OJZ2M0eS0-_Lb8pKOaLuA",
+        "name":"Aura"
+    },
+    
+]
+}
