f729cc3aa63f6558b18fbffc9adbd74b2f4454fe
Bharathhh30 <vnsb30@gmail.com>
2025-01-26 21:56:52 +0530

    checkpoint 1.8 done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,20 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 
 export default function Layout(){
     return (
         <GestureHandlerRootView >
-            <Slot/>
+            {/* <StatusBar hidden = {false} /> */}
+            <Stack screenOptions={{
+                headerShown: false
+            }} >
+                <Stack.Screen name="(nobottombars)/info" options={{headerShown:true,
+                    headerTitle: "Account Info",
+                    headerBackTitle : "Go Back"}} />
+                
+            </Stack>
         </GestureHandlerRootView>
     )
 }
